9c00c1e48ab15920c9e9d3673ce16435e1964da5
zanzydnd <zanzydnd@gmail.com>
2021-10-08 22:29:42 +0300

    management commands

---
+++ src/main/management/commands/getcategories.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+import vk_api
+
+from main.models import InterestCategory
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        session = vk_api.VkApi(token="")
+
+        vk = session.get_api()
+
+        interes = session.method("ads.getSuggestions", {"section": "interest_categories_v2", "country": 1})
+        data = list(InterestCategory(interes_name=interes[i]['name']) for i in range(len(interes)))
+        InterestCategory.objects.bulk_create(data)
