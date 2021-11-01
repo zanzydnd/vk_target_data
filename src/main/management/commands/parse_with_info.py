2ebe6a0be84d8976d2ed6f82162d9a4aac4cff13
zanzydnd <zanzydnd@gmail.com>
2021-11-01 22:59:16 +0300

    try except

--- src/main/management/commands/parse_with_info.py
+++ src/main/management/commands/parse_with_info.py
@@ -1,3 +1,4 @@
+
 import django
 from django.core.management import BaseCommand
 
@@ -6,6 +7,7 @@ django.setup()
 from multiprocessing import Pool
 from main.models import ApiKey
 from main.services import  bridge_to_async_info, butch_before_procs_info
+import sys
 
 
 class Command(BaseCommand):
@@ -13,6 +15,8 @@ class Command(BaseCommand):
         num_tokens = ApiKey.objects.filter(expired=False).count()
 
         arrays = butch_before_procs_info()
-
-        with Pool(num_tokens) as p:
-            p.map(bridge_to_async_info, arrays)
+        try:
+            with Pool(num_tokens) as p:
+                p.map(bridge_to_async_info, arrays)
+        except Exception as e:
+            print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(e).__name__, e)
